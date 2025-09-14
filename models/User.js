623b12ed48a47874ec33c85570028c6bd448ab99
Saim-Warsi <kamransaim385@gmail.com>
2025-09-14 19:13:55 +0500

    first commit

---
+++ models/User.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const userSchema = mongoose.Schema({
+    _id : {type:string, required:true},
+     username : {type:string, required:true},
+      email : {type:string, required:true},
+       image : {type:string, required:true},
+        role : {type:string, enum :["user" , "hotelowner"],default:"user"},
+        recentSearchCities : [{type:string, required:true}],
+},{timestamps:true})
+
+const User = mongoose.model("User", userSchema);
+export default User;
